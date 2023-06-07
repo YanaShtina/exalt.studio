1e5b7410484c2150d40403bb9707774dfcabcb38
Iana Shtina <sianalucky@gmail.com>
2023-06-07 22:26:34 +0300

    js

--- src/modules/slider.js
+++ src/modules/slider.js
@@ -1,7 +1,40 @@
-import Swiper, { Navigation, Pagination } from 'swiper';
+import Swiper, { Pagination } from 'swiper';
 
 export default {
   init() {
-    console.log('dfjflıdjlfj', Swiper);
+    const breakpoint = window.matchMedia( '(max-width: 1023px)' );
+    console.log('breakpoint', breakpoint.matches)
+    const swiper = new Swiper('.swiper', {
+      // configure Swiper to use modules
+      modules: [Pagination],
+      pagination: {
+        el: '.swiper-pagination',
+        type: 'bullets',
+      },
+
+      breakpoints: {
+        // when window width is >= 640px
+        767: {
+          slidesPerView: 1,
+          spaceBetween: 20
+        }
+      }
+
+    });
+
+    if (breakpoint.matches === true) {
+      swiper.destroy();
+
+      const hiddenSlide = document.querySelector('.process__slide._2');
+
+      hiddenSlide.classList.add('hidden');
+      const openBtn = document.querySelector('.more');
+      
+
+      openBtn.addEventListener('click', () => {
+        hiddenSlide.classList.remove('hidden');
+        openBtn.style.display = 'none';
+      })
+    }
   }
 }
